878460a4e267b4112702b8ac721f8b1dd1b1c70b
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2018-06-02 18:23:09 +0400

    feat: Implement ShelfSelector component

---
+++ src/main/components/ShelfSelector.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+/**
+ * Stateless component that contains options to move a book
+ * between shelfs.
+ */
+function ShelfSelector(props) {
+  const { book, onChangeShelf } = props;
+
+  /**
+   * Function to pass selected shelf into parent component
+   *
+   * @param {Event} event - an event which took place in the DOM
+   */
+  const changeShelf = event => {
+    onChangeShelf(book, event.target.value);
+  };
+
+  return (
+    <div className="book-shelf-changer">
+      <select value={book.shelf || "none"} onChange={changeShelf}>
+        <option value="none" disabled>
+          Move to...
+        </option>
+        <option value="currentlyReading">Currently Reading</option>
+        <option value="wantToRead">Want to Read</option>
+        <option value="read">Read</option>
+        <option value="none">None</option>
+      </select>
+    </div>
+  );
+}
+
+ShelfSelector.propTypes = {
+  book: PropTypes.object.isRequired,
+  onChangeShelf: PropTypes.func.isRequired
+};
+
+export default ShelfSelector;
