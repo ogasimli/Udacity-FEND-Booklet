c2e5f0c04d9b9bf0df21d06715326fff4f65bdf4
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2018-06-03 00:51:59 +0400

    feat: Implement BookShelf component

---
+++ src/main/components/BookShelf.js
@@ -0,0 +1,26 @@
+import React from "react";
+import BooksGrid from "./BooksGrid";
+import PropTypes from "prop-types";
+
+/**
+ * Stateless component that represents the grid of books.
+ */
+function BookShelf(props) {
+  const { shelf, books } = props;
+
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{shelf.title}</h2>
+      <div className="bookshelf-books">
+        <BooksGrid books={books} />
+      </div>
+    </div>
+  );
+}
+
+BookShelf.propTypes = {
+  shelf: PropTypes.object.isRequired,
+  books: PropTypes.array.isRequired
+};
+
+export default BookShelf;
