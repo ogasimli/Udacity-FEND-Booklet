758bb64d856e3228cf199e22835d55775237b386
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2018-06-02 23:47:42 +0400

    feat: Implement BooksGrid component

---
+++ src/main/components/BooksGrid.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Book from "./Book";
+import PropTypes from "prop-types";
+
+/**
+ * Stateless component that represents the grid of books.
+ */
+function BooksGrid(props) {
+  const { books } = props;
+
+  return (
+    <ol className="books-grid">
+      {books.map(book => (
+        <li key={book.id}>
+          <Book book={book} />
+        </li>
+      ))}
+    </ol>
+  );
+}
+
+BooksGrid.propTypes = {
+  book: PropTypes.array.isRequired
+};
+
+export default BooksGrid;
